a6175fd11c910b61be2162f6a89ba685d11f6f44
Allyson Camitta <allyson.camitta@gmail.com>
2020-08-23 04:21:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 function johnLennonFacts(facts) {
+  let i = 0;
   while(i < facts.length) {
     facts.splice([i], 1, `${facts[i]}!!!`; 
     i++;
